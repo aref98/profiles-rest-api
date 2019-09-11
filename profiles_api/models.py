69320eb3da902b45ca605d4dc1470e7aec39d952
Aref <atb.1394@gmail.com>
2019-09-11 11:07:03 +0430

    Enable Djanfo admin interface for user profile model

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -5,29 +5,30 @@ from django.contrib.auth.models import BaseUserManager
 
 
 class UserProfileManager(BaseUserManager):
-	"""manager for user profiles"""
-	def create_user(self,email,name,password=None):
-		"""Create a new user profile"""
-		if not email:
-			raise ValueError('User must have an email')
+    """Manager for user profiles"""
 
-		email=self.normalize_email(email)
-		user=self.model(email=email, name=name)
+    def create_user(self, email, name, password=None):
+        """Create a new user profile"""
+        if not email:
+            raise ValueError('Users must have an email address')
 
-		user.set_password(password)
-		user.save(using=self._db)
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name,)
 
-		return user
+        user.set_password(password)
+        user.save(using=self._db)
 
-		def create_superuser(self,email,name,password):
-			"""create and save a new superuser with your detail"""
-			user=self.create_user(email,name,password)
+        return user
 
-			user.is_superuser=True
-			user.is_staff=True
-			user.save(using=self._db)
+    def create_superuser(self, email, name, password):
+        """Create and save a new superuser with given details"""
+        user = self.create_user(email, name, password)
 
-			return user
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
 
 
 class UserProfile(AbstractBaseUser ,PermissionsMixin):
