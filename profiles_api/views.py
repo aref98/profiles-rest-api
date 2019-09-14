93a52c187e3326a8579b208d8e1a6ff20064e9c5
Aref <atb.1394@gmail.com>
2019-09-14 04:51:22 +0430

    added profile viewset

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,7 +2,9 @@ from rest_framework.views import APIView
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
-
+from profiles_api import models
+from rest_framework.authentication import TokenAuthentication
+from profiles_api import permissions
 from profiles_api import serializers
 
 
@@ -73,7 +75,7 @@ class HelloViewSet(viewsets.ViewSet):
 			return Response({'message':message})
 		else:
 			return Response(
-                serializer.errors,
+				serializer.errors,
 				status=status.HTTP_400_BAD_REQUEST
 			)
 
@@ -92,3 +94,10 @@ class HelloViewSet(viewsets.ViewSet):
 	def destroy(self ,request, pk=None):
 		"""handle removing an object"""
 		return Response({'http_method':'DELETE'})
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+	"""Handle creating and updating profiles"""
+	serializer_class = serializers.UserProfileSerializer
+	queryset = models.UserProfile.objects.all()
+	authentication_classes = (TokenAuthentication,)
+	permission_classes=(permissions.UpdateOwnProfile,)
