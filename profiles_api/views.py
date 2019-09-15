d0d383026b7bbafa43ab7e69cee207ac38c8c585
Aref <atb.1394@gmail.com>
2019-09-15 10:47:36 +0430

    added searching to user profile api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from profiles_api import models
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
 from profiles_api import permissions
 from profiles_api import serializers
 
@@ -101,3 +102,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 	queryset = models.UserProfile.objects.all()
 	authentication_classes = (TokenAuthentication,)
 	permission_classes=(permissions.UpdateOwnProfile,)
+	filter_backends=(filters.SearchFilter,)
+	search_fields=('name','email',)
